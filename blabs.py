f97e79dfdaa3221ec7bac37b4af43e256eafc094
Ahmed Abdelrahman <a.abdelrahman@btech.com>
2024-10-20 01:56:54 +0300

    initatil a new branch

---
+++ blabs.py
@@ -0,0 +1,21 @@
+class students:
+    no_of_std = 0
+    def __init__(self, name,age,salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        students.no_of_std += 1
+    def descripe(self):
+        return f"hello {self.name} your age is {self.age} congrats on your salary ({self.salary})"
+
+st1 = students("ahmed", 20, 5618)
+st2 = students("anwar", 21, 5618)
+
+
+print(st1.name, st1.age, st1.salary)
+print(st2.name, st2.age, st2.salary)
+print(students.no_of_std)
+print(st1.descripe())
+
+
+
